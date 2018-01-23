67f1ddae92a815c76bb08ca2824771ed1f3ffa75
Jason Michael <jason@euremir.com>
2018-01-22 23:07:24 -0500

    Updated json string with new model values

--- webservice/main.go
+++ webservice/main.go
@@ -7,8 +7,8 @@ import (
 
 // main function to boot up everything
 func main() {
-	players = append(players, Player{ID: "1", Firstname: "John", Lastname: "Doe", Address: &Address{City: "City X", State: "State X"}})
-	players = append(players, Player{ID: "2", Firstname: "Koko", Lastname: "Doe", Address: &Address{City: "City Z", State: "State Y"}})
+	players = append(players, Player{ID: "1", Firstname: "John", Lastname: "Doe", Email: "john@funding.com", Phone: "5555555555", Address: &Address{ID: 1, City: "City X", State: "State X", Zipcode: "11101"}, CreatedOn: "20180122", Active: true, SignedUp: true})
+	players = append(players, Player{ID: "2", Firstname: "Koko", Lastname: "Doe", Email: "koko@funding.com", Phone: "5555551234", Address: &Address{ID: 2, City: "City Z", State: "State Y", Zipcode: "11101"}, CreatedOn: "20180122", Active: false, SignedUp: false})
 
 	var router = NewRouter()
 
